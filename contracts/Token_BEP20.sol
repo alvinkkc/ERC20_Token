793aba7ce921fe501ee5c52149a456cc0bf211b8
"alvinkkc" <"alvinchin1114@gmail.com">
2021-12-17 16:20:14 +0800

    update IBEP20

--- contracts/Token_BEP20.sol
+++ contracts/Token_BEP20.sol
@@ -2,9 +2,9 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-import "./ERC20/ERC20.sol";
+import "./IBEP20/BEP20.sol";
 
-contract Token_Erc20 is ERC20{
+contract Token_BEP20 is BEP20{
     constructor(uint256 initialSupply) ERC20("HA Coin", "HAC", msg.sender, true, true) {
         _mint(msg.sender, initialSupply);
     }
---
+++ contracts/Token_ERC20.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./ERC20/ERC20.sol";
+
+contract Token_ERC20 is ERC20{
+    constructor(uint256 initialSupply) ERC20("HA Coin", "HAC", msg.sender, true, true) {
+        _mint(msg.sender, initialSupply);
+    }
+
+    
+}
